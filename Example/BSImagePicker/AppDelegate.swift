095666aa8fbbc689d91da9bbaa21bf0f09bf4257
ToyStory <supanat_toy@hotmail.com>
2018-04-30 00:09:07 +0700

    set navigation bar title color

--- Example/BSImagePicker/AppDelegate.swift
+++ Example/BSImagePicker/AppDelegate.swift
@@ -28,6 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UIBarButtonItem.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
         return true
     }
 }
